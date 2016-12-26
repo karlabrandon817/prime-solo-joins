1743ee42b9c1f98f93635b35834733a516554db5
Karla Hugo <karlabrandon817@gmail.com>
2016-12-26 13:57:50 -0600

    found solutions

--- joins-solution.sql
+++ joins-solution.sql
@@ -4,10 +4,33 @@ JOIN addresses ON customers.id = addresses.customer_id;
 
 -- 2. Get all orders and their line items.
 SELECT * FROM orders
-JOIN line_items ON  orders.id = line_items.order_id;
+JOIN line_items ON orders.id = line_items.order_id;
 
 -- 3. Which warehouses have cheetos?
 SELECT warehouse.warehouse FROM warehouse
 JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
 JOIN products ON warehouse_product.product_id = products.id
 where products.description = 'cheetos';
+
+-- 4. Which warehouses have diet pepsi?
+SELECT warehouse.warehouse FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+JOIN products ON warehouse_product.product_id = products.id
+where products.description = 'diet pepsi';
+
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT customers.first_name, customers.last_name, count(orders.id) FROM customers
+JOIN addresses ON customers.id = addresses.customer_id
+JOIN orders ON addresses.id = orders.address_id
+GROUP BY customers.id;
+
+-- 6. How many customers do we have?
+SELECT count (id) FROM customers;
+
+-- 7. How many products do we carry?
+SELECT count (id) FROM products;
+
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT sum (on_hand) FROM warehouse_product
+JOIN products ON warehouse_product.product_id = products.id
+WHERE products.description = 'diet pepsi';
